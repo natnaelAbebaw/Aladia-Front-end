a25092924744513e736c07f08317d533757443fd
natnaelAbebaw <natnaell70a@gmail.com>
2024-11-10 14:54:18 +0300

    add repo name

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,9 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
+  app: {
+    baseURL: "/your-repo-name/", // Replace 'your-repo-name' with the name of your GitHub repository
+  },
+
   // Nuxt generates a static version of the app
 });
