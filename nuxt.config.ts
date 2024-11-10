9362ec4ad360adbac7d606e8692a63786f971e79
natnaelAbebaw <natnaell70a@gmail.com>
2024-11-10 12:37:31 +0300

    Deploy to GitHub Pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,11 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
+  target: "static", // Nuxt generates a static version of the app
+  router: {
+    base: "Aladia-Front-end", // Replace 'your-repo-name' with your GitHub repository name
+  },
+  generate: {
+    fallback: true, // Generates a 404.html for pages not found
+  },
 });
